bd41473c358e2b07f36a7cd84927ca8f0cdf97a5
piyushnbali <46628672+piyushnbali@users.noreply.github.com>
2019-08-19 19:04:05 +0530

    this sends many mail to client

---
+++ mail.py
@@ -0,0 +1,33 @@
+import smtplib
+
+def sendemail():
+    GMAIL_USERNAME = "priyawagh5151@gmail.com"
+    GMAIL_PASSWORD = "xvsvsvsrrgr00@" # your password goes here
+
+    recipient = "saish3147@gmail.com"
+    body_of_email = "Hi i liked your profile and interseted to catch up with you "
+    email_subject = "It's a match from tinder"
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+
+    # message to be sent
+    headers = "\r\n".join(["from: " + GMAIL_USERNAME,
+                           "subject: " + email_subject,
+                           "to: " + recipient,
+                           "mime-version: 1.0",
+                           "content-type: text/html"])
+
+    content = headers + "\r\n\r\n" + body_of_email
+    s.sendmail(GMAIL_USERNAME, recipient, content)
+    s.quit()
+
+for i in range(100):
+
+ sendemail()
